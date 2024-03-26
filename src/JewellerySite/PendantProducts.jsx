c2988065832251a788b493a6a2685d12a9feb020
Nitin Jalindar Handal <133699263+nitin331@users.noreply.github.com>
2024-03-26 11:49:43 +0530

    Update PendantProducts.jsx

--- src/JewellerySite/PendantProducts.jsx
+++ src/JewellerySite/PendantProducts.jsx
@@ -79,7 +79,7 @@ const PendantProducts = ({ products, addToCart }) => {
     <div>
       <div className="container">
         <center>
-          <h1>Mens Collection</h1>
+          <h1>Pendants</h1>
         </center>
         <div className="btn">
           <Button onClick={toggleDrawer(true)}>Filter</Button>
